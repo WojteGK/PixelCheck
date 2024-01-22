c9f5891f3f8c68cdcab0c306459133749825a42c
WojteGK <93033932+WojteGK@users.noreply.github.com>
2024-01-22 21:42:53 +0100

    Refactor Board class to include methods for getting and removing pieces

--- src/main/java/board.java
+++ src/main/java/board.java
@@ -7,8 +7,15 @@ public class Board {
         positions = new Position[8][8];
         for (int x = 0; x < 8; x++) {
             for (int y = 0; y < 8; y++) {
-                positions[x][y] = new Position(x, y);
+                positions[x][y] = new Position(x, y, null);
             }
         }
     }
+    
+    public Piece getPieceAt(Position position) {
+        return positions[position.getX()][position.getY()].getPiece();
+    }
+   public void removePieceAt(Position newPosition) {
+        positions[newPosition.getX()][newPosition.getY()].setPiece(null);
+   }
 }
