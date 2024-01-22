53e0a2b7d5607df0f693dd31321dd8fe69eac9c4
WojteGK <93033932+WojteGK@users.noreply.github.com>
2024-01-22 21:41:57 +0100

    Refactor Piece class constructor and isValidMove method

--- src/main/java/piece.java
+++ src/main/java/piece.java
@@ -1,26 +1,20 @@
 package src.main.java;
 
 public abstract class Piece {
-    private Position position;
+    
     private boolean isWhite;
 
-    public Piece(Position position, boolean isWhite) {
-        this.position = position;
+    public Piece( boolean isWhite) {
+        
         this.isWhite = isWhite;
-    }
-
-    public Position getPosition() {
-        return position;
-    }
-
-    public void setPosition(Position position) {
-        this.position = position;
-    }
+    }   
 
     public boolean isWhite() {
         return isWhite;
     }
 
     // This method should be overridden by each specific piece type
-    public abstract boolean isValidMove(Position newPosition);
+    public abstract boolean isValidMove(Position oldPosition, Position newPosition);
+
+    public abstract char getSymbol();
 }
